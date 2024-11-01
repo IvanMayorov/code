ef122b1d9729a94553828adb5163ec681d8d07d1
IvanMayorov <mailmayorov@gmail.com>
2024-11-01 21:21:14 +0300

    Update cityads_main.js

--- cityads_main.js
+++ cityads_main.js
@@ -1,3 +1,97 @@
+
+const swiper3d = new Swiper(".s2_main_section", {
+  slidesPerView: 5,
+      centeredSlides: true,
+      watchSlidesProgress: true,
+      loop: true,
+    //   observer: true,
+    //   longSwipesRatio: 10,
+    //   loopAdditionalSlides: 20,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        bulletActiveClass: "bullet-active",
+        bulletClass: "pagination-bullet",
+      },
+    //   effect: 'coverflow',
+    //   coverflowEffect: {
+    //     rotate: 0,
+    //     stretch: 50,
+    //     depth: 100,
+    //     modifier: 3,
+    //     scale: 1,
+    //     slideShadows: true,
+    // },
+      on: {
+        // touchEnd(swiper) {
+        //     swiper.update();
+        // },
+        click(swiper) {
+          console.log(swiper.activeIndex);
+          if (swiper.clickedIndex < swiper.activeIndex) {
+            swiper.slidePrev();
+          } else if (swiper.clickedIndex > swiper.activeIndex) {
+            swiper.slideNext();
+          }
+        },
+
+        // eslint-disable-next-line
+        progress(swiper) {
+        //   console.log("slideEl");
+          const scaleStep = 0.15;
+          const zIndexMax = swiper.slides.length;
+          for (let i = 0; i < swiper.slides.length; i += 1) {
+            const slideEl = swiper.slides[i];
+            const slideProgress = swiper.slides[i].progress;
+            const absProgress = Math.abs(slideProgress);
+            let modify = 1;
+            if (absProgress > 1) {
+              modify = (absProgress - 1) * 0.3 + 1;
+            }
+  
+            const translate = `${slideProgress * modify * 45}%`;
+            const scale = 1 - absProgress * scaleStep;
+            const zIndex = zIndexMax - Math.abs(Math.round(slideProgress));
+            slideEl.style.transform = `translateX(${translate}) scale(${scale}) `;
+            slideEl.style.zIndex = zIndex;
+            if (absProgress > 3) {
+              slideEl.style.opacity = 0;
+            } else {
+              slideEl.style.opacity = 1;
+            }
+  
+            const opacityEls = slideEl.querySelector(".overlay");
+            opacityEls.style.opacity = `${absProgress * 0.3}`;
+          }
+        },
+        // eslint-disable-next-line
+        setTransition(swiper, duration) {
+          for (let i = 0; i < swiper.slides.length; i += 1) {
+            const slideEl = swiper.slides[i];
+            const opacityEls = slideEl.querySelectorAll(":scope > div");
+            slideEl.style.transitionDuration = `${duration}ms`;
+            opacityEls.forEach((opacityEl) => {
+              opacityEl.style.transitionDuration = `${duration}ms`;
+            });
+          }
+        },
+      },
+    });
+
+
+  gsap.to(".main_hero_section", {
+    scrollTrigger: {
+      trigger: ".hero-sticky_wrap",
+      start: "top top",
+      end: "bottom top",
+      scrub: true,
+    },
+    filter: "blur(24px)",
+    opacity: 0,
+  });
+
+
+
 //Navbar________________________________________________________________________
 
 const dropdowns = document.querySelectorAll(".navbar_dropdown_box");
@@ -36,39 +130,6 @@ const handleClickOutside = (event) => {
 // Добавляем обработчик события клика на каждую кнопку
 dropdownButtons.forEach((button, index) => {
   button.addEventListener("click", (event) => {
-    event.preventDefault(); // Предотвращаем скролл страницы
-    event.stopPropagation(); // Останавливаем всплытие события
-    event.stopImmediatePropagation(); // Останавливаем выполнение других обработчиков
-
-    const dropdown = dropdowns[index];
-    const isVisible = dropdown.style.display === "block";
-
-    if (isVisible) {
-      closeDropdowns();
-    } else {
-      // Закрываем все другие dropdown перед открытием нового
-      closeDropdowns();
-      
-      // Если скрыто, показываем
-      dropdown.style.display = "block"; // Сначала показываем элемент
-      gsap.to(dropdown, {
-        autoAlpha: 1,
-        y: 0, // При открытии смещение к нулю
-        duration: 0.3,
-        onComplete: () => {
-          dropdown.style.display = "block"; // Убедимся, что элемент остается видимым после анимации
-        }
-      });
-      // Удаляем обработчик клика вне элемента после открытия
-      document.removeEventListener("click", handleClickOutside);
-      document.addEventListener("click", handleClickOutside); // Создаем обработчик при открытии
-    }
-  });
-});
-
-// Добавляем обработчик события для мобильных устройств
-dropdownButtons.forEach((button, index) => {
-  button.addEventListener("touchstart", (event) => {
     event.preventDefault(); // Предотвращаем скролл страницы
     event.stopPropagation(); // Останавливаем всплытие события
     event.stopImmediatePropagation(); // Останавливаем выполнение других обработчиков
