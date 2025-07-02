b6f3af47169057e8dc14a2d6d94051ae0ec1f5f4
IvanMayorov <mailmayorov@gmail.com>
2025-07-02 10:33:02 +0300

    Update jinx.js

--- jinx.js
+++ jinx.js
@@ -1547,7 +1547,7 @@ ScrollTrigger.create({
   
   // #region FOOTER
 
-//   const footer = document.querySelector(".footer");
+  const footer = document.querySelector(".footer");
  
   
 // gsap.to([mobileMask, document.querySelector('.navbar'), navLinksBox], {
@@ -1561,18 +1561,18 @@ ScrollTrigger.create({
 //   }
 // });
 
-// gsap.to(".flames_box", {
-//   height: '18rem',
+gsap.to(".flames_box", {
+  height: '18rem',
 
-//   scrollTrigger: {
-//     trigger: footer,
-//     start: "top 50%",
-//     end: "bottom bottom",
-//     scrub: 1,
-//     immediateRender: false,
-//     // markers: true
-//   }
-// });
+  scrollTrigger: {
+    trigger: footer,
+    start: "top 50%",
+    end: "bottom bottom",
+    scrub: 1,
+    immediateRender: false,
+    // markers: true
+  }
+});
 
 
 // #endregion
