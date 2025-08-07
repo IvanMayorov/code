6917f49222346688f362d840a6a2d0d09940d734
IvanMayorov <mailmayorov@gmail.com>
2025-08-07 20:42:21 +0300

    Update boss.js

--- boss.js
+++ boss.js
@@ -1,8 +1,13 @@
-console.log('DOMContentLoaded');
+
 const soundBtn = document.querySelector('.sound_btn');
 const soundText = soundBtn.querySelector('.sound_text');
 const soundOnIcon = soundBtn.querySelector('.sound_on');
 const soundOffIcon = soundBtn.querySelector('.sound_off');
+
+const audioUrl = 'https://res.cloudinary.com/do7m7foqv/video/upload/v1754586487/Final_boss_tayess.mp3';
+const audio = new Audio(audioUrl);
+audio.loop = true;
+audio.preload = 'auto';
 
 let isSoundOn = false;
 
@@ -24,4 +29,10 @@ updateSoundButton();
 soundBtn.addEventListener('click', function() {
   isSoundOn = !isSoundOn;
   updateSoundButton();
+  if (isSoundOn) {
+    audio.play();
+  } else {
+    audio.pause();
+    audio.currentTime = 0;
+  }
 });
